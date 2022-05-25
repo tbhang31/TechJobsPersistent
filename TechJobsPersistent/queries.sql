54ea5ac18e461007fa3484b1996145ce1b42564d
Tom Hang <tbhang31@gmail.com>
2022-05-25 14:11:12 -0400

    updating the queries.sql

--- TechJobsPersistent/queries.sql
+++ TechJobsPersistent/queries.sql
@@ -1,6 +1,19 @@
 --Part 1
 
+Columns:
+Id int AI PK 
+Name longtext 
+EmployerId int
+
 --Part 2
+
+# Id	Name	Location
+1	John	Milkwakee
+2	Jenna	Philadelphia
+3	Gerard	St. Louis
+4	Grace	Dallas
+5	Claire	Atlanta
+6	Courtney	Penn Interactive
 
 --Part 3
 
