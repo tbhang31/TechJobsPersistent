18c77e46df7380035b3c9816457392fe449b3b3d
Tom Hang <tbhang31@gmail.com>
2022-05-26 16:40:51 -0400

    Wahoo

--- TechJobsPersistent/queries.sql
+++ TechJobsPersistent/queries.sql
@@ -11,3 +11,9 @@ SELECT * FROM techjobs.employers where location="st louis"
 
 --Part 3
 
+SELECT * 
+FROM techjobs.skills
+LEFT JOIN techjobs.jobskills ON techjobs.skills.Id = techjobs.jobskills.SkillId
+RIGHT JOIN techjobs.jobs ON techjobs.jobskills.JobId = techjobs.jobs.Id
+WHERE techjobs.jobs.Name IS NOT NULL
+ORDER BY techjobs.jobs.Name ASC
